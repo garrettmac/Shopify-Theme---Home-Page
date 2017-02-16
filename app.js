34e62eb8a39aa7fa360eccc6361f7dff401dcc74
Vyga <garrettmacmac@gmail.com>
2017-02-16 13:53:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
  $(document).ready(function () {
 // ------------- VARIABLES ------------- //
+console.clear()
 var ticking = false;
 var isFirefox = (/Firefox/i.test(navigator.userAgent));
 var isIe = (/MSIE/i.test(navigator.userAgent)) || (/Trident.*rv\:11\./i.test(navigator.userAgent));
-var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive)
+var scrollSensitivitySetting = 30; //Increase/decrease this number to change sensitivity to trackpad gestures (up = less sensitive; down = more sensitive) 
 var slideDurationSetting = 600; //Amount of time for which slide is "locked"
 var currentSlideNumber = 0;
 var totalSlideNumber = $(".background").length;
 
 // ------------- DETERMINE DELTA/SCROLL DIRECTION ------------- //
 function parallaxScroll(evt) {
-  if (isFirefox) {
-    //Set delta for Firefox
-    delta = evt.detail * (-120);
-  } else if (isIe) {
-    //Set delta for IE
-    delta = -evt.deltaY;
-  } else {
-    //Set delta for all other browsers
-    delta = evt.wheelDelta;
-  }
+  // $(window).on("mousewheel DOMMouseScroll", function(event){
+  // event.stopPropagation();  event.preventDefault(); 
+  // console.log("evt",evt)
+  // if(_.has(evt,'originalEvent.wheelDelta')){
+    // var delta  =evt.originalEvent.wheelDelta/120
+  // }
+  // else if(_.has(evt,'originalEvent.detail')){
+    // var delta  =-evt.originalEvent.detail/3
+  // }
+  // else if(_.has(evt,'deltaY')){
+    var delta  =-evt.deltaY///3
+  // }
+   // console.log("evt.detail",evt.detail)
+  // console.log("delta",delta)
+  $(document).ready(function() {
+    $('#toptitle').text(function(i, oldText) {
+        return oldText === delta ?  "not set":delta ;
+    });
+});
+  // turn(Del);
+// });
+  // if (isFirefox) {
+  //   //Set delta for Firefox
+  //   delta = evt.detail * (-120);
+  // } else if (isIe) {
+  //   //Set delta for IE
+  //   delta = -evt.deltaY;
+  //   console.log("deltaY: ",delta)
+  // } else {
+  //   //Set delta for all other browsers
+  //   delta = evt.wheelDelta;
+  //  // delta = evt.originalEvent.wheelDelta
+  //   console.log("delta: ",delta)
+  // }
 
   if (ticking != true) {
     if (delta <= -scrollSensitivitySetting) {
@@ -51,11 +76,26 @@ function slideDurationTimeout(slideDuration) {
 }
 
 // ------------- ADD EVENT LISTENER ------------- //
-var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+ var mousewheelEvent = isFirefox ? "DOMMouseScroll" : "wheel";
+//var mousewheelEvent = "mousewheel DOMMouseScroll"
+
+
+//   var $fwindow = $(window);
+//   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+// console.log("scrollTop",scrollTop)
+//   // on window scroll event
+//   $fwindow.on('scroll resize', function() {
+//     scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+//   }); 
+// var throttled = _.throttle(renewToken, 300000, { 'trailing': false });
+// jQuery(element).on('click', throttled);
+// console.log("parallaxScroll",parallaxScroll)
+// var trot=_.throttle(parallaxScroll, 60)
+// console.log("trot",trot)
 window.addEventListener(mousewheelEvent, _.throttle(parallaxScroll, 60), false);
 
 // ------------- SLIDE MOTION ------------- //
-function nextItem() { 
+function nextItem() {
   var $previousSlide = $(".background").eq(currentSlideNumber - 1);
   $previousSlide.removeClass("up-scroll").addClass("down-scroll");
 }
@@ -64,5 +104,5 @@ function previousItem() {
   var $currentSlide = $(".background").eq(currentSlideNumber);
   $currentSlide.removeClass("down-scroll").addClass("up-scroll");
 }
-
-})
+   
+ })
